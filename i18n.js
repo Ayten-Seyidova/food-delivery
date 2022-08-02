e45175c0123952c3046195901fde66f853a3d065
Ayten-Seyidova <you@example.com>
2022-08-02 16:31:05 +0400

    create language system

---
+++ i18n.js
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./lang/en.json";
+import fr from "./lang/fr.json";
+import az from "./lang/az.json";
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en,
+      fr,
+      az,
+    },
+
+    lng: "en",
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
